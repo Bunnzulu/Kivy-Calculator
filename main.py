8a1f287b2bea5d5fbcea76cb973df1f9bc40e192
Nobody <bunnza2020@gmail.com>
2024-07-25 21:48:17 -0500

    Layout

---
+++ main.py
@@ -0,0 +1,17 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+
+
+
+class CalculatorApp(App):
+    pass
+
+class MainScreen(BoxLayout):
+    pass
+
+
+
+
+
+CalculatorApp().run()
