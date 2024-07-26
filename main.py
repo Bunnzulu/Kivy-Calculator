cc8817ee914b476dc7a44d431e46cd5fd7a5ff82
Nobody <bunnza2020@gmail.com>
2024-07-26 11:11:47 -0500

    Display trouble

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
-
-
+from kivy.properties import StringProperty
 
 class CalculatorApp(App):
     pass
@@ -10,15 +9,38 @@ class CalculatorApp(App):
 class MainScreen(BoxLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.N1 = 0
-        self.N2 = 0
+        self.N1 = ""
+        self.N2 = ""
         self.Operater = ""
+        self.Answer = 0
+        self.Display_Text = StringProperty("-")
 
     def Number_Button(self,Number):
-        print(Number)  
+        if self.N1 == "":
+            self.N1 = int(Number)
+            self.Display_Text = Number
+        elif self.Operater != "":
+            self.N2 = int(Number)
+            self.Display_Text += Number
     
-    def Operater_Buttons(self,op):print(op)
-
+    def Operater_Buttons(self,op):
+        if self.Operater == "":
+            self.Operater = op
+            self.Display_Text += op
+    
+    def Reset(self):
+        self.N1 = ""
+        self.N2 = ""
+        self.Operater = ""
+    
+    def Calculate(self):
+        if self.N2 != "":
+            if self.Operater == "+": self.Answer = self.N1 + self.N2
+            elif self.Operater == "-": self.Answer = self.N1 - self.N2
+            elif self.Operater == "*": self.Answer = self.N1 * self.N2
+            elif self.Operater == "/": self.Answer = self.N1/self.N2
+            self.Reset()
+            self.Display_Text = self.Answer
 
 
 
